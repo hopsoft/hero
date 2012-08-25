8795c38dee9f4efa418693e8d2e0f1856c56aa57
Nathan Hopkins <natehop@gmail.com>
2012-08-25 14:29:52 -0600

    Added docs to the logger setter.

--- lib/hero.rb
+++ lib/hero.rb
@@ -4,6 +4,13 @@ end
 
 module Hero
   class << self
+
+    # You can optionally register a Logger with Hero.
+    # If set, a logger.info message will be written for each step called when running Hero::Formulas.
+    #
+    # @example
+    #   Hero.logger = Logger.new(STDOUT)
     attr_accessor :logger
+
   end
 end
